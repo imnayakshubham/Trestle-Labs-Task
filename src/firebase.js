69af188d9ffbd46efdeec59c521ed42e9a3e70c0
Nayak Shubham <70691188+imnayakshubham@users.noreply.github.com>
2021-09-01 21:04:46 +0530

    Removed API keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8XpuNxobMTlltaKWul8S5BjKR0-JTbcM",
-  authDomain: "trestle-f477b.firebaseapp.com",
-  projectId: "trestle-f477b",
-  storageBucket: "trestle-f477b.appspot.com",
-  messagingSenderId: "361758485488",
-  appId: "1:361758485488:web:ffe67ec6aec7ab2e6c4b35",
+  //api key
 };
 const app = firebase.initializeApp(firebaseConfig);
 
